32e77b502e98294f6cedc6f1b4d62aec1dbe4376
Kelly Lougheed <kellylougheed@gmail.com>
2018-05-02 03:27:11 +0000

    Add secret canvas

--- draw.js
+++ draw.js
@@ -12,15 +12,21 @@ var c;
 
 var bell;
 var ting;
+var magic;
 
 var paused = false;
+
+var canvas;
+var hue = 0;
 
 function preload() {
     bell = loadSound('Good-idea-bell.mp3');
     ting = loadSound('Ting-sound-effect.mp3');
+    magic = loadSound('Magical-chime-sound-effect.mp3');
 }
 
 function setup() {
+    canvas = false;
     createCanvas(WIDTH, HEIGHT);
     colors = [color(255, 0, 0), 
             color(0, 255, 0), 
@@ -35,14 +41,19 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+    if (!canvas) {
+        background(0);
+    }
+    
+    canvas ? fill(255, 0, 255) : fill(255);
     textSize(10);
 	textFont('Verdana');
 	var speed = Math.abs(xChange.toFixed(1));
 	var txt = "SPACE: pause/play, UP: faster, DOWN: slower (Speed: " + speed + ")";
+	txt += "      ** sound on **       ENTER: access/escape secret canvas";
 	text(txt, 2, HEIGHT-2);
 	
-    if (!paused) {
+    if (!paused && !canvas) {
         x += xChange;
     	y += yChange;
     }
@@ -63,6 +74,12 @@ function draw() {
 	textSize(36);
 	text("DVD", x + (BOX_SIDE*0.12), y + (BOX_SIDE*0.6));
 	fill(255);
+	
+	if (canvas) {
+	    fill(c);
+	    textSize(24);
+	    text("Draw on me!", 10, 40);
+	}
   
 }
 
@@ -78,15 +95,26 @@ function keyPressed() {
     if (keyCode === UP_ARROW) {
         xChange *= 1.1;
         yChange *= 1.1;
-      } else if (keyCode === DOWN_ARROW) {
+    } else if (keyCode === DOWN_ARROW) {
         xChange *= 0.9;
         yChange *= 0.9;
-      } else if (keyCode === 32) {
-          if (paused) {
-              paused = false;
-          } else {
-              paused = true;
-          }
-      }
-      return false;
+    } else if (keyCode === 32) {
+        paused ? paused = false : paused = true;
+    } else if (keyCode === ENTER) {
+        canvas ? canvas = false : canvas = true;
+        if (canvas) {
+            background(255);
+            magic.setVolume(0.1);
+            magic.play();
+        }
+    }
+    return false;
+}
+
+function mouseDragged() {
+    colorMode(HSL, 255);
+    hue < 255 ? hue += 5 : hue = 0;
+    fill(hue, 200, 150);
+    noStroke();
+    ellipse(mouseX, mouseY, 30, 30);
 }
